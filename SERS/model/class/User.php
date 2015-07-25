bd93abc498b791d3948b9b7b2c13aabeb7be89c6
Alexis <Alexis@Alexis-PC-Port>
2015-07-26 00:52:36 +0100

    Création des attributs dans model/class/User.php

--- SERS/model/class/User.php
+++ SERS/model/class/User.php
@@ -1,16 +1,96 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
  * Description of User
- *
+ * 
  * @author Alexis
  */
+
 class User {
-    //put your code here
+    
+    ///////////////
+    // ATTRIBUTS //
+    ///////////////
+    
+    /*
+     * @var int
+     */
+    private $id;
+    
+    /*
+     * @var string
+     */
+    private $prenom;
+    
+    /*
+     * @var string
+     */
+    private $nom;
+    
+    /*
+     * @var string
+     */
+    private $mail;
+    
+    /*
+     * @var string
+     */    
+    private $adresse;
+
+    /*
+     * @var date
+     */
+    private $date_naissance;
+
+    /*
+     * @var date
+     */
+    private $date_creation;
+    
+    
+    /*
+     * @var string
+     */
+    private $pseudo;
+    
+    /*
+     * @var string
+     */
+    private $password;
+
+    /*
+     * @var bool
+     */
+    private $affiche;
+    
+    
+    ///////////////////
+    // CONSTRUCTEURS //
+    ///////////////////
+           
+    public function classeBidon($id=-1, $nom='', $dateDeNaissance=null, $solde=0, $vivant=false)
+    {
+        $this->id               = $id;
+        $this->nom              = $nom;
+        $this->dateDeNaissance  = $dateDeNaissance;
+        $this->solde            = $solde;
+        $this->vivant           = $vivant;        
+    }
+    
+    /////////////////////
+    // GETTERS&SETTERS //
+    /////////////////////
+    
+    public function setId($id)
+    {
+        if(is_int($id))
+        {
+            $this->id = $id;
+        }
+    }
+    
+    public function getId()
+    {
+        return $this->id;
+    }
 }
