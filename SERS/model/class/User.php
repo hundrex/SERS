1007dc3b516050e14441b835fa94230c5a019375
Alexis <Alexis@Alexis-PC-Port>
2015-07-26 01:34:30 +0100

    Ajout des getter et setter pour User

--- SERS/model/class/User.php
+++ SERS/model/class/User.php
@@ -63,6 +63,15 @@ class User {
      */
     private $affiche;
     
+    /*
+     * @var Fichier
+     */
+    private $extFichier;
+    
+    /*
+     * @var TypeUtilisateur
+     */
+    private $extTypeUtilisateur;
     
     ///////////////////
     // CONSTRUCTEURS //
@@ -81,6 +90,7 @@ class User {
     // GETTERS&SETTERS //
     /////////////////////
     
+    //GetterSetter id
     public function setId($id)
     {
         if(is_int($id))
@@ -92,5 +102,189 @@ class User {
     public function getId()
     {
         return $this->id;
+    }
+    
+    //GetterSetter prenom
+        public function setPrenom($prenom)
+    {
+        if(is_string($prenom))
+        {
+            $this->prenom = $prenom;
+        }
+    }
+    
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+    
+    //GetterSetter nom
+    public function setNom($nom)
+    {
+        if(is_string($nom))
+        {
+            $this->nom = $nom;
+        }
+    }
+    
+    public function getNom()
+    {
+        return $this->nom;
+    }
+    
+    //GetterSetter mail
+    public function setMail($mail)
+    {
+        if(is_string($mail))
+        {
+            $this->mail = $mail;
+        }
+    }
+    
+    public function getMail()
+    {
+        return $this->mail;
+    }
+    
+    //GetterSetter adresse
+    public function setAdresse($adresse)
+    {
+        if(is_string($adresse))
+        {
+            $this->adresse = $adresse;
+        }
+    }
+    
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+    
+    //GetterSetter dateNaissance
+    public function setDateNaissance($dateNaissance)
+    {
+        if(is_a($dateNaissance, "Date"))
+        {
+            $this->dateNaissance = $dateNaissance;
+        }
+    }
+    
+    public function getDateNaissance()
+    {
+        return $this->dateNaissance;
+    }
+    
+    //GetterSetter dateCreation
+    public function setDateCreation($dateCreation)
+    {
+        if(is_a($dateCreation, "Date"))
+        {
+            $this->dateCreation = $dateCreation;
+        }
+    }
+    
+    public function getDateCreation()
+    {
+        return $this->dateCreation;
+    }   
+    
+    //GetterSetter pseudo
+    public function setPseudo($pseudo)
+    {
+        if(is_string($pseudo))
+        {
+            $this->pseudo = $pseudo;
+        }
+    }
+    
+    public function getPseudo()
+    {
+        return $this->pseudo;
+    }
+    
+    //GetterSetter password
+    public function setPassword($password)
+    {
+        if(is_string($password))
+        {
+            $this->password = $password;
+        }
+    }
+    
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    //GetterSetter affiche
+    public function setAffiche($affiche)
+    {
+        if(is_bool($affiche))
+        {
+            $this->affiche = $affiche;
+        }
+    }
+    
+    public function getAffiche()
+    {
+        return $this->affiche;
+    }
+    
+    //GetterSetter fichier
+    public function setFichier($fichier)
+    {
+        if(is_int($fichier))
+        {
+            $this->extFichier = FichierDAL::findById($fichier);
+        }
+        else if(is_a($fichier, "Fichier"))
+        {
+            $this->extFichier = $fichier;
+        }
+    }
+    
+    public function getFichier()
+    {
+        $fichier = null;
+        
+        if(is_int($this->extFichier))
+        {
+            $fichier = FichierDAL::findById($this->extFichier);
+            $this->extFichier = $fichier;
+        }
+        else if(is_a($this->extFichier, "Fichier"))
+        {
+            $fichier = $this->extFichier;
+        }
+        return $fichier;
+    }
+    
+    //GetterSetter extTypeUtilisateur
+    public function setTypeUtilisateur($extTypeUtilisateur)
+    {
+        if(is_int($extTypeUtilisateur))
+        {
+            $this->extTypeUtilisateur = TypeUtilisateurDAL::findById($extTypeUtilisateur);
+        }
+        else if(is_a($extTypeUtilisateur, "TypeUtilisateur"))
+        {
+            $this->extTypeUtilisateur = $extTypeUtilisateur;
+        }
+    }
+    
+    public function getTypeUtilisateur()
+    {
+        $extTypeUtilisateur = null;
+        
+        if(is_int($this->extTypeUtilisateur))
+        {
+            $extTypeUtilisateur = TypeUtilisateurDAL::findById($this->extTypeUtilisateur);
+            $this->extTypeUtilisateur = $extTypeUtilisateur;
+        }
+        else if(is_a($this->extTypeUtilisateur, "TypeUtilisateur"))
+        {
+            $extTypeUtilisateur = $this->extTypeUtilisateur;
+        }
+        return $extTypeUtilisateur;
     }
 }
