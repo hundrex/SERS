80af59941916cbe1fda324f0a68fd4506800e706
lucile-sticky <lucile@stickyads.tv>
2015-07-27 09:22:40 +0100

    Formating.

--- SERS/model/class/Travail.php
+++ SERS/model/class/Travail.php
@@ -1,51 +1,46 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of Travail
  *
  * @author Alexis
  */
 class Travail {
+
     ///////////////
     // ATTRIBUTS //
     ///////////////
-    
+
     /*
      * @var int
      */
     protected $id;
-    
+
     /*
      * @var string
      */
     protected $label;
-   
+
     /*
      * @var date
      */
     protected $dateCreation;
-        
+
     /*
      * @var date
      */
     protected $datePassage;
-     
+
     /*
      * @var string
      */
     protected $description;
-    
+
     /*
      * @var int
      */
     protected $annee;
-    
+
     /*
      * @var int
      */
@@ -55,20 +50,17 @@ class Travail {
      * @var bool
      */
     protected $affiche;
-    
+
     /*
      * @var Module
      */
     protected $extModule;
-    
-    
+
     ///////////////////
     // CONSTRUCTEURS //
     ///////////////////
-           
-    public function Travail($id=-1, $label=null,$number=null,
-            $description=null,$dateCreation=null,$datePassage=null,
-            $annee=null, $affiche=null, $extModule=null, $prixRattrapage=null)
+
+    public function Travail($id = -1, $label = null, $number = null, $description = null, $dateCreation = null, $datePassage = null, $annee = null, $affiche = null, $extModule = null, $prixRattrapage = null)
     {
         $this->id = $id;
         $this->label = $label;
@@ -78,122 +70,113 @@ class Travail {
         $this->affiche = $affiche;
         $this->number = $number;
         $this->annee = $annee;
-        $this->prixRattrapage =$prixRattrapage;
+        $this->prixRattrapage = $prixRattrapage;
         $this->extModule = $extModule;
     }
-    
+
     /////////////////////
     // GETTERS&SETTERS //
     /////////////////////
-    
-    //GetterSetter id
+
     public function setId($id)
     {
-        if(is_int($id))
+        if (is_int($id))
         {
             $this->id = $id;
         }
     }
-    
+
     public function getId()
     {
         return $this->id;
     }
 
-    //GetterSetter prixRattrapage
     public function setPrixRattrapage($prixRattrapage)
     {
-        if(is_int($prixRattrapage))
+        if (is_int($prixRattrapage))
         {
             $this->prixRattrapage = $prixRattrapage;
         }
     }
-    
+
     public function getPrixRattrapage()
     {
         return $this->prixRattrapage;
     }
-    
-    //GetterSetter number
+
     public function setNumber($number)
     {
-        if(is_int($number))
+        if (is_int($number))
         {
             $this->number = $number;
         }
     }
-    
+
     public function getNumber()
     {
         return $this->number;
     }
-    
-    //GetterSetter dateCreation
+
     public function setDateCreation($dateCreation)
     {
-        if(is_a($dateCreation, "Date"))
+        if (is_a($dateCreation, "Date"))
         {
             $this->dateCreation = $dateCreation;
         }
     }
-    
+
     public function getDateCreation()
     {
         return $this->dateCreation;
-    }   
-    
-    
-    //GetterSetter datePassage
+    }
+
     public function setDatePassage($datePassage)
     {
-        if(is_a($datePassage, "Date"))
+        if (is_a($datePassage, "Date"))
         {
             $this->datePassage = $datePassage;
         }
     }
-    
+
     public function getDatePassage()
     {
         return $this->datePassage;
-    }    
-    
-    //GetterSetter label
+    }
+
     public function setLabel($label)
     {
-        if(is_string($label))
+        if (is_string($label))
         {
             $this->label = $label;
         }
     }
-    
+
     public function getLabel()
     {
         return $this->label;
     }
-    
-    //GetterSetter description
+
     public function setDescription($description)
     {
-        if(is_string($description))
+        if (is_string($description))
         {
             $this->description = $description;
         }
     }
-    
+
     public function getDescription()
     {
         return $this->description;
     }
-    
-     //GetterSetter affiche
+
     public function setAffiche($affiche)
     {
-        if(is_bool($affiche))
+        if (is_bool($affiche))
         {
             $this->affiche = $affiche;
         }
     }
-    
+
     public function getAffiche()
     {
         return $this->affiche;
@@ -202,36 +185,36 @@ class Travail {
     //GetterSetter module
     public function setModule($module)
     {
-        if(is_int($module))
+        if (is_int($module))
         {
             $this->extModule = ModuleDAL::findById($module);
         }
-        else if(is_a($module, "Module"))
+        else if (is_a($module, "Module"))
         {
             $this->extModule = $module;
         }
     }
-    
+
     public function getModule()
     {
         $module = null;
-        
-        if(is_int($this->extModule))
+
+        if (is_int($this->extModule))
         {
             $module = ModuleDAL::findById($this->extModule);
             $this->extModule = $module;
         }
-        else if(is_a($this->extModule, "Module"))
+        else if (is_a($this->extModule, "Module"))
         {
             $module = $this->extModule;
         }
         return $module;
     }
-    
+
     //////////////
     // METHODES //
     //////////////
-    
+
     protected function hydrate($dataSet)
     {
         $this->id = $dataSet['id'];
@@ -243,4 +226,5 @@ class Travail {
         $this->annee = $dataSet['annee'];
         $this->extModule = $dataSet['Module'];
     }
+
 }
