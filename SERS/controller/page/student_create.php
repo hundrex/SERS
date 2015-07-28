2a55350d15b65649bc151fd76e6a8581a23f7fd1
Alexis <Alexis@Alexis-PC-Port>
2015-07-28 13:32:59 +0100

    Correction student_create.php (controller), j'avais oublié de mettre a jour le module après insertion de l'étudiant

--- SERS/controller/page/student_create.php
+++ SERS/controller/page/student_create.php
@@ -64,6 +64,8 @@ for ($i = 0; $i < $N; $i++)
     $moduleId = (int) $unModule[$i]; //recup l'id du module select, le cast en int et le stock  
     $module = ModuleDAL::findById($moduleId); //recherche le module correspondant à partir de son id
     $module->inscrireEleve($student); //inscrit dans ce module le student qui a était précédemment créer
+    ModuleDAL::insertOnDuplicate($module); //met a jout le module avec son nouveau etudiant inscrit
     echo "Ajout de ". $student->getNom()."dans le module ".$module->getLabel()."</br>";
 }
+
 
