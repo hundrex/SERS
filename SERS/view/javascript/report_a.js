999a0938aa1bafb2c539d3edd2f1c836189cacd1
lucile-sticky <lucile@stickyads.tv>
2015-07-28 19:29:38 +0100

    Bonnes couleurs Ref #1

--- SERS/view/javascript/report_a.js
+++ SERS/view/javascript/report_a.js
@@ -19,22 +19,48 @@ function chargerGraph(data) {
             type: 'column'
         },
         title: {
-            text: 'Fruit Consumption'
+            text: 'Student marks'
         },
         xAxis: {
-            categories: ['Apples', 'Bananas', 'Oranges']
+            categories: ['Web Dev', 'M2', 'M3', 'M4']
         },
         yAxis: {
             title: {
                 text: 'Fruit eaten'
             }
         },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}:   </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} %</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
         series: [{
-            name: 'Jane',
-            data: [1, 0, 4]
+            name: 'Final',
+            data: [
+                {y : 77.5, color: 'green'}, 
+                {y : 78.5, color: 'green'}, 
+                {y : 92, color: 'green'}, 
+                {y : 47, color: 'red'}
+            ]
         }, {
-            name: 'John',
-            data: [5, 7, 3]
+            name: 'Assignment',
+            data: [
+                {y : 100, color: '#72AE00'}, 
+                {y : 80, color: '#72AE00'}, 
+                {y : 100, color: '#72AE00'}, 
+                {y : 40, color: '#FF5454'}
+            ]
+        }, {
+            name: 'Exam',
+            data: [
+                {y : 55, color: '#FF5454'}, 
+                {y : 77, color: '#72AE00'}, 
+                {y : 84, color: '#72AE00'}, 
+                {y : 54, color: '#FF5454'}
+            ]
         }]
     });
 };
