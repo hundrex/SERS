bd5e61c6f21398967c0b6c36b05315c2828ffbca
hundrix <hundrix@MainServ>
2015-07-29 20:04:28 +0100

    report_b rework de la récup des données

--- SERS/controller/ajax/report_b.php
+++ SERS/controller/ajax/report_b.php
@@ -7,46 +7,67 @@ $categories[] = 'test2';
 $categories[] = 'test3';
 
 $data = array();
-$finalMarks = array();
-$finalMarks[] = array('y' => 77.5, 'color' => 'green');
-$finalMarks[] = array('y' => 78.5, 'color' => 'green');
-$finalMarks[] = array('y' => 92, 'color' => 'green');
-$finalMarks[] = array('y' => 47, 'color' => 'red');
-$finalMarks[] = array('y' => 77.5, 'color' => 'green');
-$finalMarks[] = array('y' => 78.5, 'color' => 'green');
-$finalMarks[] = array('y' => 92, 'color' => 'green');
-$finalMarks[] = array('y' => 47, 'color' => 'red');
-$final = new Serie('Final', $finalMarks);
 
-$assignmentMarks = array();
-$assignmentMarks[] = array('y' => 100, 'color' => '#72AE00');
-$assignmentMarks[] = array('y' => 80, 'color' => '#72AE00');
-$assignmentMarks[] = array('y' => 100, 'color' => '#72AE00');
-$assignmentMarks[] = array('y' => 40, 'color' => '#FF5454');
-$assignmentMarks[] = array('y' => 100, 'color' => '#72AE00');
-$assignmentMarks[] = array('y' => 80, 'color' => '#72AE00');
-$assignmentMarks[] = array('y' => 100, 'color' => '#72AE00');
-$assignmentMarks[] = array('y' => 40, 'color' => '#FF5454');
-$assign = new Serie('Assignment', $assignmentMarks);
+$databaseData = array();
+$databaseData[] = array(100,55);
+$databaseData[] = array(80,77);
+$databaseData[] = array(100,84);
+$databaseData[] = array(40,54);
+$databaseData[] = array(100,55);
+$databaseData[] = array(80,77);
+$databaseData[] = array(100,84);
+$databaseData[] = array(40,54);
 
-$examMarks = array();
-$examMarks[] = array('y' => 55, 'color' => '#FF5454');
-$examMarks[] = array('y' => 77, 'color' => '#72AE00');
-$examMarks[] = array('y' => 84, 'color' => '#72AE00');
-$examMarks[] = array('y' => 54, 'color' => '#FF5454');
-$examMarks[] = array('y' => 55, 'color' => '#FF5454');
-$examMarks[] = array('y' => 77, 'color' => '#72AE00');
-$examMarks[] = array('y' => 84, 'color' => '#72AE00');
-$examMarks[] = array('y' => 54, 'color' => '#FF5454');
-$exam = new Serie('Exam', $examMarks);
+$assignmentData = array();
+$assignmentReturn = array();
+$examData = array();
+$examReturn = array();
+$finalData = array();
+$finalReturn = array();
+
+foreach ($databaseData as $row) {
+    $assignMark = $row[0];
+    $assignmentData["y"] = $assignMark;
+    if ($assignMark >= 60) {
+        $color = '#72AE00';
+    }
+    else {
+        $color = '#FF5454';
+    }
+    $assignmentData["color"] = $color;
+    $assignmentReturn[] = $assignmentData;
+    
+    $examMark = $row[1];
+    $examData["y"] = $examMark;
+    if ($examMark >= 60) {
+        $color = '#72AE00';
+    }
+    else {
+        $color = '#FF5454';
+    }
+    $examData["color"] = $color;
+    $examReturn[] = $examData;
+    
+    $finalMark = ($assignMark + $examMark)/2;
+    $finalData["y"] = $finalMark;
+    if ($finalMark >= 60) {
+        $color = 'green';
+    }
+    else {
+        $color = 'red';
+    }
+    $finalData["color"] = $color;
+    $finalReturn[] = $finalData;
+}
+
+$exam = new Serie('Exam', $examReturn);
+$assignment = new Serie('Assignment', $assignmentReturn);
+$final = new Serie('Final', $finalReturn);
 
 $data[] = $final;
-$data[] = $assign;
+$data[] = $assignment;
 $data[] = $exam;
 
 $bigdata = array($categories, $data);
-
-
 $data_json = json_encode($bigdata);
-
 echo $data_json;
