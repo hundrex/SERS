b96d6ce0d3da5d238042d33a8b7921c788f41ee6
lucile-sticky <lucile@stickyads.tv>
2015-07-29 02:06:11 +0100

    Les data sont générées (mais non dynamiquement) via l'ajax !

--- SERS/controller/ajax/report_a.php
+++ SERS/controller/ajax/report_a.php
@@ -1,5 +1,33 @@
 <?php
-$data = 'Hello world';
+
+require_once '../../model/class/Serie.php';
+
+$data = array();
+$finalMarks = array();
+$finalMarks[] = array('y' => 77.5, 'color' => 'green');
+$finalMarks[] = array('y' => 78.5, 'color' => 'green');
+$finalMarks[] = array('y' => 92, 'color' => 'green');
+$finalMarks[] = array('y' => 47, 'color' => 'red');
+$final = new Serie('Final', $finalMarks);
+
+$assignmentMarks = array();
+$assignmentMarks[] = array('y' => 100, 'color' => '#72AE00');
+$assignmentMarks[] = array('y' => 80, 'color' => '#72AE00');
+$assignmentMarks[] = array('y' => 100, 'color' => '#72AE00');
+$assignmentMarks[] = array('y' => 40, 'color' => '#FF5454');
+$assign = new Serie('Assignment', $assignmentMarks);
+
+$examMarks = array();
+$examMarks[] = array('y' => 55, 'color' => '#FF5454');
+$examMarks[] = array('y' => 77, 'color' => '#72AE00');
+$examMarks[] = array('y' => 84, 'color' => '#72AE00');
+$examMarks[] = array('y' => 54, 'color' => '#FF5454');
+$exam = new Serie('Exam', $examMarks);
+
+$data[] = $final;
+$data[] = $assign;
+$data[] = $exam;
+
 $data_json = json_encode($data);
 
 echo $data_json;
