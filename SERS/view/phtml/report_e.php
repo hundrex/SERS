c8ee0eb6b0dd38d2b288bb33d8bd53f7ade2c858
hundrix <hundrix@MainServ>
2015-07-30 11:56:02 +0100

    report_e begin dynamisme

--- SERS/view/phtml/report_e.php
+++ SERS/view/phtml/report_e.php
@@ -1,36 +1,44 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/SERS/SERS/model/DAL/UserDAL.php'); ?>
+
 <div class="row filter-bar">
     <div class="col-lg-6">
         <div class="input-group">
+            <?php $modules = ModuleDAL::findAll(); ?> 
             <span class="input-group-addon" id="moduleSelecterReportE">Module</span>
             <select class="form-control">
-                <option value="webDevelopment">Web Development</option>
-                <option value="webDesign">Web Design</option>
-                <option value="contentManagementSystem">Content Management System</option>
-                <option value="legalEthicalSocialAndProfessionalIssues">Legal Ethical Social and Professional Issues</option>
-                <option value="webDevelopmentFramework">Web Development Framework</option>
+                <?php foreach ($modules as $module): ?>
+                    <option value="<?php echo $module->getId(); ?>">
+                        <?php echo $module->getLabel(); ?>
+                    </option>
+                <?php endforeach; ?>
             </select>
         </div>
     </div>
     <div class="col-lg-6">
         <button type="button" class="btn btn-default btn-right"><span class="glyphicon glyphicon-print"></span></button>
-        <button type="button" class="btn btn-default btn-right">Display</button>
     </div>
 </div>
 
-
+<?php
+$mesStudents = UserDAL::findAllStudent();
+?>
 <div class="panel panel-default">
     <div class="panel-heading">
-        <h3 class="panel-title">Web development</h3>
+        <h3 class="panel-title">
+            <?php
+                echo $modules[0]->getLabel();
+            ?>
+        </h3>
     </div>
-        <table class="table">
-            <tr>
-                <th>Last Name</th><th>First Name</th><th>Result</th>
-            </tr>
-            <tr>
-                <td>Durden</td><td>Taylor</td><td>Fail</td>
-            </tr>
-            <tr>
-                <td>Anderson</td><td>Thomas</td><td>Pass</td>
-            </tr>
-        </table>
+    <table class="table">
+        <tr>
+            <th>Last Name</th><th>First Name</th><th>Result</th>
+        </tr>
+        <?php
+        ?>
+        <tr>
+            <td>Durden</td><td>Taylor</td><td>Fail</td>
+        </tr>
+
+    </table>
 </div>
