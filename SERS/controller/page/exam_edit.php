0e3df93aa9f235e5f524ad246609fba77b1f4e31
Alexis <Alexis@Alexis-PC-Port>
2015-07-30 15:53:10 +0100

    modif label et desc d'un exam

--- SERS/controller/page/exam_edit.php
+++ SERS/controller/page/exam_edit.php
@@ -13,16 +13,19 @@ require_once '../../model/DAL/AssignmentDAL.php';
 require_once '../../model/class/Module.php';
 require_once '../../model/DAL/ModuleDAL.php';
 
-$validExamId = filter_input(INPUT_POST, 'exam_id', FILTER_SANITIZE_STRING);
+$validExamId = filter_input(INPUT_GET, 'exam_id', FILTER_SANITIZE_STRING);
 $validExamId = (int) $validExamId;
 $exam = ExamDAL::findById($validExamId);
 
-$validLabel = filter_input(INPUT_POST, 'exam_label', FILTER_SANITIZE_STRING);
+$validModuleId = filter_input(INPUT_GET, 'module_id', FILTER_SANITIZE_STRING);
+$validModuleId = (int)$validModuleId;
+
+$validLabel = filter_input(INPUT_GET, 'exam_label', FILTER_SANITIZE_STRING);
 $exam->setLabel($validLabel);
 
-$validDesc = filter_input(INPUT_POST, 'exam_desc', FILTER_SANITIZE_STRING);
+$validDesc = filter_input(INPUT_GET, 'exam_desc', FILTER_SANITIZE_STRING);
 $exam->setDescription($validDesc);
 
-ExamDAL::insertOnDuplicate($exam);
+ExamDAL::insertOnDuplicate($exam,$validModuleId);
 
 echo '<META HTTP-EQUIV="Refresh" Content="0; URL=../../?page=module&modification=success">';
